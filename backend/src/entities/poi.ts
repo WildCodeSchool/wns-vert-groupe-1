fd2f85fb6ae5101ea8e723bc27fe2058bd959bad
Yuliia Kolomiets <ykolomiets@mac100003.home>
2024-01-31 17:36:09 +0100

    add get cityById and getAllCities resolvers

--- backend/src/entities/poi.ts
+++ backend/src/entities/poi.ts
@@ -1,5 +1,12 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+} from "typeorm";
 import { Field } from "type-graphql";
+import { City } from "./city";
 
 @Entity()
 export class Poi extends BaseEntity {
@@ -22,4 +29,7 @@ export class Poi extends BaseEntity {
   @Field(() => [String])
   @Column("text", { array: true, default: [] })
   images: string[];
+
+  @ManyToOne(() => City, (city) => city.pois, { nullable: true })
+  city: City;
 }
--- backend/src/resolvers/City.ts
+++ backend/src/resolvers/City.ts
@@ -1,11 +1,34 @@
 import { City } from "../entities/city";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class CityResolver {
   @Query(() => [City])
-  async allCities() {
-    const result = await City.find();
+  async getAllCities() {
+    const result = await City.find({ relations: { pois: true } });
     return result;
+  }
+
+  @Query(() => City)
+  async getCityById(@Arg("id") id: number): Promise<City> {
+    try {
+      const result = await City.findOne({
+        where: {
+          id: id,
+        },
+        relations: { pois: true },
+      });
+
+      if (!result) {
+        throw new Error(`Poi with ID ${id} not found`);
+      }
+
+      console.log(result);
+
+      return result;
+    } catch (err) {
+      console.error("Error", err);
+      throw new Error("An error occurred while reading one poi");
+    }
   }
 }
