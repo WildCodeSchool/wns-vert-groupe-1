9ac07778fe4f261f15de850b940a8107864f4f6b
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-01-31 17:53:38 +0100

    no more error, black screen

--- frontend/src/components/mapbox-map.tsx
+++ frontend/src/components/mapbox-map.tsx
@@ -1,42 +1,31 @@
-import * as React from "react";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css"; 
-// import the mapbox-gl styles so that the map is displayed correctly
+import React, { useState, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function MapboxMap() {
-    // this is where the map instance will be stored after initialization
-  const [map, setMap] = React.useState<mapboxgl.Map>();
+  const [map, setMap] = useState<mapboxgl.Map | undefined>(undefined);
+  const mapNode = useRef<HTMLDivElement>(null);
 
-    // React ref to store a reference to the DOM node that will be used
-  // as a required parameter `container` when initializing the mapbox-gl
-  // will contain `null` by default
-    const mapNode = React.useRef(null);
-
-  React.useEffect(() => {
+  useEffect(() => {
     const node = mapNode.current;
-        // if the window object is not found, that means
-        // the component is rendered on the server
-        // or the dom node is not initialized, then return early
-    if (typeof window === "undefined" || node === null) return;
+    if (!window || !node) return;
 
-        // otherwise, create a map instance
     const mapboxMap = new mapboxgl.Map({
       container: node,
-            accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
-            style: "mapbox://styles/mapbox/streets-v11",
+      accessToken:'pk.eyJ1IjoibWVpamU4IiwiYSI6ImNsczF1ZXlqczBjeW4yanBjZzNsbXFuZncifQ.7Z0qk6v18gniDPLKIctVQA',
+      style: 'mapbox://styles/mapbox/streets-v11',
       center: [-74.5, 40],
       zoom: 9,
     });
 
-        // save the map object to React.useState
     setMap(mapboxMap);
 
-        return () => {
+    return () => {
       mapboxMap.remove();
     };
   }, []);
 
-    return <div ref={mapNode} style={{ width: "100%", height: "100%" }} />;
+  return <div ref={mapNode} style={{ width: '100%', height: '100%' }} />;
 }
 
-export default MapboxMap
+export default MapboxMap;
