d4a4daf85d73e5708f6d3dbc977d2ed67cc1a35a
sonm0002 <duygu.sonmez@hotmail.fr>
2024-02-01 14:50:20 +0100

    add hot reload

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: false,
+	webpack: (config) => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300,
+		};
+		return config;
+	},
+};
 
 export default nextConfig;
