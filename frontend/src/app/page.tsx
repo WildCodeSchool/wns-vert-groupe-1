267388ee0d3899657b428350a58c4f0c243da965
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-02-01 11:08:13 +0100

    WIP docker reload

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,26 @@
-'use client'
+// page.js
+"use client";
 
-import MapboxMap from "../components/mapbox-map";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-function App() {
-  return <MapboxMap />;
+import classes from "./Page.module.css";
+
+export default function Home() {
+	const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+	return (
+    <>
+    <h5>cc</h5>
+		<main className={classes.mainStyle}>
+			<Map
+				mapboxAccessToken={mapboxToken}
+				mapStyle="mapbox://styles/mapbox/streets-v12"
+				initialViewState={{ latitude: 35.668641, longitude: 139.750567, zoom: 10 }}
+				maxZoom={20}
+				minZoom={3}
+			></Map>
+		</main>
+    </>
+	);
 }
-
-export default App;
