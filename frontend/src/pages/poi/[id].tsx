cd0fc39b745db2c7f5e96626b01531526e54d30f
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-02-08 16:45:32 +0100

    added caroussel

--- frontend/src/pages/poi/[id].tsx
+++ frontend/src/pages/poi/[id].tsx
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router"
 import { useQuery } from "@apollo/client";
-import { Breadcrumbs, Typography, Grid, Card, CardMedia, CardContent } from '@mui/material';
+import Carousel from 'react-material-ui-carousel'
+
+import { Breadcrumbs, Typography, Grid, Card, CardMedia, CardContent, Paper } from '@mui/material';
 
 import { useEffect, useState } from "react";
 import { GET_POI_BY_ID } from "@queries";
 import { POIInput } from "@types";
 
 const POIDetails = () => {
+    
     const router = useRouter();
     const [POI, setPOI] = useState<POIInput>({
         name: "",
@@ -34,7 +37,14 @@ console.log(data)
         }
     }, [data, error, router.query.id]);
     console.log(POIDetails)
-
+    
+    function Item(props: any) {
+        return (
+            <Paper>
+                <img src={props.item} alt={`Image ${props.index}`} style={{ width: '100%' }} />
+            </Paper>
+        )
+    }
 return (
     <div>
         <Breadcrumbs aria-label="breadcrumb">
@@ -42,23 +52,12 @@ return (
             <Typography color="textPrimary">{POI.category}</Typography>
         </Breadcrumbs>
         <Grid container spacing={2}>
-            <Grid item xs={6}>
-                <div style={{ width: "100%" }}>
-                    {POI.images.map((image, index) => (
-                        <Card key={index}>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image={image}
-                                alt={`Image ${index}`}
-                            />
-                            <CardContent>
-                                {/* You can add any additional content for each image here */}
-                            </CardContent>
-                        </Card>
-                    ))}
-                </div>
-            </Grid>
+                <Carousel>
+                    {
+                    POI.images.map((imageUrl, i) => <Item key={i} item={imageUrl} index={i} />)
+                    }
+                </Carousel>
+             
             <Grid item xs={6}>
                 <Typography variant="h4">{POI.name}</Typography>
                 Adresse :
