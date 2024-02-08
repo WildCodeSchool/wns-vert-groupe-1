4ecf32821dfebec27b3ffb364087febd78eecb10
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-02-08 16:21:37 +0100

    display POi infos

--- frontend/src/pages/poi/[id].tsx
+++ frontend/src/pages/poi/[id].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import { useQuery } from "@apollo/client";
+import { Breadcrumbs, Typography, Grid, Card, CardMedia, CardContent } from '@mui/material';
 
 import { useEffect, useState } from "react";
 import { GET_POI_BY_ID } from "@queries";
@@ -7,7 +8,7 @@ import { POIInput } from "@types";
 
 const POIDetails = () => {
     const router = useRouter();
-    const [POIDetails, setPOIDetails] = useState<POIInput>({
+    const [POI, setPOI] = useState<POIInput>({
         name: "",
         address: "",
         description: "",
@@ -22,17 +23,53 @@ const POIDetails = () => {
 console.log(data) 
    useEffect(() => {
         if (!loading && data && data.getPoiById) {
-            setPOIDetails({
+            setPOI({
                 name: data.getPoiById.name,
                 address: data.getPoiById.address,
                 description: data.getPoiById.description,
                 images: data.getPoiById.images,
-                city: data.getPoiById.city,
-                category: data.getPoiById.category,
+                city: data.getPoiById.city.name,
+                category: data.getPoiById.category.name,
             });
         }
     }, [data, error, router.query.id]);
     console.log(POIDetails)
+
+return (
+    <div>
+        <Breadcrumbs aria-label="breadcrumb">
+            <Typography color="textPrimary">{POI.city}</Typography>
+            <Typography color="textPrimary">{POI.category}</Typography>
+        </Breadcrumbs>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <div style={{ width: "100%" }}>
+                    {POI.images.map((image, index) => (
+                        <Card key={index}>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={image}
+                                alt={`Image ${index}`}
+                            />
+                            <CardContent>
+                                {/* You can add any additional content for each image here */}
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="h4">{POI.name}</Typography>
+                Adresse :
+                <Typography>{POI.address}</Typography>
+                Description : 
+                <Typography>{POI.description}</Typography>
+            </Grid>
+        </Grid>
+    </div>
+);
 }
+
 
 export default POIDetails;
