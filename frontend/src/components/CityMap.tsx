486833d3a53a83b9eb52c80c88759e55da9776eb
Yuliia Kolomiets <ykolomiets@mac100003.home>
2024-03-13 15:14:12 +0100

    add mapbox style link

--- frontend/src/components/CityMap.tsx
+++ frontend/src/components/CityMap.tsx
@@ -5,62 +5,48 @@ export type CityMapProps = {
 	lat?: number;
 	lon?: number;
 	pois?: {
-	  id: number;
-	  name: string;
-	  address: string;
-	  description: string;
-	  latitude: number;
-	  longitude: number;
+		id: number;
+		name: string;
+		address: string;
+		description: string;
+		latitude: number;
+		longitude: number;
 	}[];
-  };
-
-  const CityMap = ({ lat, lon, pois }: CityMapProps) => {
-    console.log("lat:", lat);
-    console.log("lon:", lon);
-    console.log("POI data:", pois);
-
-    const key = `${lat}-${lon}`;
-
-    return (
-        <>
-            <Map
-                key={key}
-                mapboxAccessToken="pk.eyJ1IjoibWVpamU4IiwiYSI6ImNsczF1ZXlqczBjeW4yanBjZzNsbXFuZncifQ.7Z0qk6v18gniDPLKIctVQA"
-                latitude={lat}
-                longitude={lon}
-                zoom={10}
-                style={{ width: 1000, height: 500 }}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-            >
-{pois && pois.length > 0 && pois.map((poi) => (
-    (poi.latitude !== undefined && poi.longitude !== undefined) && (
-        <Marker
-            key={poi.id}
-            latitude={poi.latitude}
-            longitude={poi.longitude}
-        >
-            <div
-                style={{
-                    width: "20px",
-                    height: "20px",
-                    borderRadius: "50%",
-                    backgroundColor: "red",
-                }}
-            ></div>
-            <div
-                style={{
-                    color: "black",
-                    fontWeight: "bold",
-                }}
-            >
-                {poi.name}
-            </div>
-        </Marker>
-    )
-))}
-            </Map>
-        </>
-    );
 };
-  export default CityMap;
-  
+
+const CityMap = ({ lat, lon, pois }: CityMapProps) => {
+	console.log("lat:", lat);
+	console.log("lon:", lon);
+	console.log("POI data:", pois);
+
+	const key = `${lat}-${lon}`;
+
+	return (
+		<>
+			<Map
+				key={key}
+				mapboxAccessToken="pk.eyJ1IjoibWVpamU4IiwiYSI6ImNsczF1ZXlqczBjeW4yanBjZzNsbXFuZncifQ.7Z0qk6v18gniDPLKIctVQA"
+				latitude={lat}
+				longitude={lon}
+				zoom={10}
+				style={{ width: 1000, height: 500 }}
+				mapStyle="mapbox://styles/mapbox/streets-v9"
+			>
+				{pois &&
+					pois.length > 0 &&
+					pois.map(
+						(poi) =>
+							poi.latitude !== undefined &&
+							poi.longitude !== undefined && (
+								<Marker
+									key={poi.id}
+									latitude={poi.latitude}
+									longitude={poi.longitude}
+								/>
+							)
+					)}
+			</Map>
+		</>
+	);
+};
+export default CityMap;
