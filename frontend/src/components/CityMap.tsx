468ea36b88bafe5e537b887dceff06018dd2565d
Yuliia Kolomiets <ykolomiets@mac100003.home>
2024-05-15 10:24:29 +0200

    search page part 2

--- frontend/src/components/CityMap.tsx
+++ frontend/src/components/CityMap.tsx
@@ -1,9 +1,25 @@
 "use client";
 import { Map, Marker } from "react-map-gl";
 import { CityType } from "@types";
+import { mainTheme } from "@theme";
 
-export const CityMap = ({ lat, lon, pois }: CityType) => {
+type CityMapProps = CityType & {
+	activePoiId: number | null;
+	onMarkerClick: (id: number) => void;
+};
+
+export const CityMap = ({
+	lat,
+	lon,
+	pois,
+	activePoiId,
+	onMarkerClick,
+}: CityMapProps) => {
 	const key = `${lat}-${lon}`;
+
+	const handleMarkerClick = (poiId: number) => {
+		onMarkerClick(poiId);
+	};
 
 	return (
 		<div style={{ flex: 1 }}>
@@ -25,7 +41,22 @@ export const CityMap = ({ lat, lon, pois }: CityType) => {
 									key={poi.id}
 									latitude={poi.latitude}
 									longitude={poi.longitude}
-								/>
+									onClick={() => handleMarkerClick(poi.id)}
+								>
+									<svg
+										height={poi.id === activePoiId ? "30" : "25"}
+										viewBox="0 0 24 24"
+										style={{
+											cursor: "pointer",
+											fill: mainTheme.palette.primary.light,
+											opacity: poi.id === activePoiId ? 1 : 0.63,
+											stroke: "none",
+											transform: "translate(-12px, -24px)",
+										}}
+									>
+										<circle cx="12" cy="12" r="10" />
+									</svg>
+								</Marker>
 							)
 					)}
 			</Map>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,79 @@
 import { SearchForm } from "@components";
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+import { Box, Stack } from "@mui/material";
+import { mainTheme } from "@theme";
 
 const Home = () => {
-	return <SearchForm />;
+	return (
+		<Stack
+			direction="row"
+			alignItems="center"
+			justifyContent="space-between"
+			flex="1"
+			spacing={1}
+			sx={{ height: "100%" }}
+		>
+			<Box
+				sx={{
+					paddingLeft: mainTheme.spacing(10),
+					height: "100%",
+					alignItems: "flex-start",
+				}}
+			>
+				<Typography
+					sx={{
+						color: mainTheme.palette.primary.contrastText,
+						fontSize: mainTheme.typography.h1,
+						fontWeight: "bold",
+						marginBottom: mainTheme.spacing(3),
+					}}
+				>
+					EXPLOREZ
+				</Typography>
+				<Typography
+					sx={{
+						color: mainTheme.palette.primary.main,
+						fontSize: mainTheme.typography.h1,
+						fontWeight: "bold",
+						marginBottom: mainTheme.spacing(4),
+					}}
+				>
+					PARIS
+				</Typography>
+				<Typography
+					sx={{
+						color: mainTheme.palette.primary.contrastText,
+						fontSize: mainTheme.typography.h2,
+						fontWeight: "bold",
+						marginBottom: mainTheme.spacing(8),
+					}}
+				>
+					COMME UN LOCAL
+				</Typography>
+				<SearchForm />
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					width: "50%",
+				}}
+			>
+				<img
+					style={{
+						width: "100%",
+						height: "100%",
+						objectFit: "cover",
+					}}
+					src="/images/homepage.png"
+					alt="View from nature"
+				/>
+			</Box>
+		</Stack>
+	);
 };
 
 export default Home;
