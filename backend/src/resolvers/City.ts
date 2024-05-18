0c564dd7097675574031efc83b9628ec53ec37a4
Yuliia Kolomiets <ykolomiets@mac100003.home>
2024-05-18 08:59:18 +0200

    add redis

--- backend/src/resolvers/City.ts
+++ backend/src/resolvers/City.ts
@@ -1,98 +1,105 @@
 import { City } from "../entities";
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { CityUpdateInput, CityInput } from "../inputs";
+import { redisClient } from "../index";
 
 @Resolver()
 export class CityResolver {
-	@Query(() => [City])
-	async getAllCities() {
-		const result = await City.find({ relations: ["pois"] });
-		return result;
-	}
+  @Query(() => [City])
+  async getAllCities() {
+    const result = await City.find({ relations: ["pois"] });
+    return result;
+  }
 
-	@Query(() => City)
-	async getCityById(@Arg("id") id: number): Promise<City> {
-		try {
-			const result = await City.findOne({
-				where: {
-					id: id,
-				},
-				relations: { pois: true },
-			});
+  @Query(() => City)
+  async getCityById(@Arg("id") id: number): Promise<City> {
+    try {
+      const result = await City.findOne({
+        where: {
+          id: id,
+        },
+        relations: { pois: true },
+      });
 
-			if (!result) {
-				throw new Error(`City with ID ${id} not found`);
-			}
+      if (!result) {
+        throw new Error(`City with ID ${id} not found`);
+      }
 
-			return result;
-		} catch (err) {
-			console.error("Error", err);
-			throw new Error("An error occurred while reading one city");
-		}
-	}
+      return result;
+    } catch (err) {
+      console.error("Error", err);
+      throw new Error("An error occurred while reading one city");
+    }
+  }
 
-	@Query(() => City)
-	async getCityByName(@Arg("name") name: string): Promise<City> {
-		try {
-			const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
-			const result = await City.findOne({
-				where: {
-					name: capitalizedName,
-				},
-				relations: ["pois", "pois.category"],
-			});
+  @Query(() => City)
+  async getCityByName(@Arg("name") name: string): Promise<City> {
+    try {
+      const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+      const cacheResult = await redisClient.get(capitalizedName);
+      if (cacheResult !== null) {
+        console.log("from cache");
+        return JSON.parse(cacheResult);
+      } else {
+        const result = await City.findOne({
+          where: {
+            name: capitalizedName,
+          },
+          relations: ["pois", "pois.category"],
+        });
 
-			if (!result) {
-				throw new Error(`City with name ${name} not found`);
-			}
+        if (!result) {
+          throw new Error(`City with name ${name} not found`);
+        }
+        redisClient.set(capitalizedName, JSON.stringify(result), { EX: 30 });
+        return result;
+      }
+    } catch (err) {
+      console.error("Error", err);
+      throw new Error("An error occurred while searching for a city by name");
+    }
+  }
 
-			return result;
-		} catch (err) {
-			console.error("Error", err);
-			throw new Error("An error occurred while searching for a city by name");
-		}
-	}
+  @Mutation(() => City)
+  async createNewCity(@Arg("cityData") cityData: CityInput) {
+    const pois = cityData.pois ? cityData.pois.map((poi) => ({ id: poi })) : [];
 
-	@Mutation(() => City)
-	async createNewCity(@Arg("cityData") cityData: CityInput) {
-		const pois = cityData.pois ? cityData.pois.map((poi) => ({ id: poi })) : [];
+    const city = await City.create({
+      ...cityData,
+      pois: pois,
+    }).save();
 
-		const city = await City.create({
-			...cityData,
-			pois: pois,
-		}).save();
+    return city;
+  }
 
-		return city;
-	}
+  @Mutation(() => String)
+  async deleteCityById(@Arg("id") id: number) {
+    const cityToDelete = await City.findOneByOrFail({
+      id: id,
+    });
+    cityToDelete.remove();
 
-	@Mutation(() => String)
-	async deleteCityById(@Arg("id") id: number) {
-		const cityToDelete = await City.findOneByOrFail({
-			id: id,
-		});
-		cityToDelete.remove();
+    return "The city has been deleted";
+  }
 
-		return "The city has been deleted";
-	}
+  @Mutation(() => City)
+  async updateCity(
+    @Arg("id") id: number,
+    @Arg("cityData") cityData: CityUpdateInput
+  ) {
+    const existingCity = await City.findOneOrFail({
+      where: { id },
+    });
 
-	@Mutation(() => City)
-	async updateCity(
-		@Arg("id") id: number,
-		@Arg("cityData") cityData: CityUpdateInput
-	) {
-		const existingCity = await City.findOneOrFail({
-			where: { id },
-		});
+    if (!existingCity) {
+      throw new Error(`City with ID ${id} not found`);
+    }
 
-		if (!existingCity) {
-			throw new Error(`City with ID ${id} not found`);
-		}
+    const updatedCity = await City.save({
+      ...existingCity,
+      ...cityData,
+    });
 
-		const updatedCity = await City.save({
-			...existingCity,
-			...cityData,
-		});
-
-		return updatedCity;
-	}
+    return updatedCity;
+  }
 }
