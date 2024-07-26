87a157864d627754a22e529d9c6582ecd2f0e03d
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-07-26 09:46:38 +0200

    fixed docker compose

--- frontend/src/components/CityMap.tsx
+++ frontend/src/components/CityMap.tsx
@@ -21,10 +21,11 @@ export const CityMap = ({
 		onMarkerClick(poiId);
 	};
 
+
 	return (
 		<Map
 			key={key}
-			mapboxAccessToken="pk.eyJ1IjoibWVpamU4IiwiYSI6ImNsczF1ZXlqczBjeW4yanBjZzNsbXFuZncifQ.7Z0qk6v18gniDPLKIctVQA"
+			mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
 			latitude={lat}
 			longitude={lon}
 			zoom={10}
