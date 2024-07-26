f3f622eafa05da9ae4c2f40b5405a306cf6032cc
sonm0002 <duygu.sonmez@hotmail.fr>
2024-07-26 16:59:30 +0200

    wip city entity

--- backend/src/entities/city.ts
+++ backend/src/entities/city.ts
@@ -1,10 +1,10 @@
 import {
-  BaseEntity,
-  Column,
-  Entity,
-  OneToMany,
-  PrimaryGeneratedColumn,
-  Unique,
+	BaseEntity,
+	Column,
+	Entity,
+	OneToMany,
+	PrimaryGeneratedColumn,
+	Unique,
 } from "typeorm";
 import { ObjectType, Field, Float } from "type-graphql";
 import { IsLatitude, IsLongitude, IsNotEmpty, Length } from "class-validator";
@@ -15,41 +15,41 @@ import { User } from "./user";
 @Entity()
 @Unique(["name"])
 export class City extends BaseEntity {
-  @Field()
-  @PrimaryGeneratedColumn()
-  id: number;
+	@Field()
+	@PrimaryGeneratedColumn()
+	id: number;
 
-  @Field()
-  @Column({ unique: true })
-  @Length(1, 50, {
-    message: "City name should have between 1 and 50 characters.",
-  })
-  name: string;
+	@Field()
+	@Column({ unique: true })
+	@Length(1, 50, {
+		message: "City name should have between 1 and 50 characters.",
+	})
+	name: string;
 
-  @Field(() => Float)
-  @Column("float")
-  @IsLatitude({ message: "Latitude should have a valid value." })
-  lat: number;
+	@Field(() => Float)
+	@Column("float")
+	@IsLatitude({ message: "Latitude should have a valid value." })
+	lat: number;
 
-  @Field(() => Float)
-  @Column("float")
-  @IsLongitude({ message: "Longitude should have a valid value." })
-  lon: number;
+	@Field(() => Float)
+	@Column("float")
+	@IsLongitude({ message: "Longitude should have a valid value." })
+	lon: number;
 
-  @Field()
-  @Column()
-  @IsNotEmpty()
-  @Length(50, undefined, {
-    message: "City description should have minimum 50 characters.",
-  })
-  description: string;
+	@Field()
+	@Column()
+	@IsNotEmpty()
+	@Length(50, undefined, {
+		message: "City description should have minimum 50 characters.",
+	})
+	description: string;
 
-  @Field(() => [Poi])
-  @OneToMany(() => Poi, (poi) => poi.city, { nullable: true })
-  pois: Poi[];
+	@Field(() => [Poi], { nullable: true })
+	@OneToMany(() => Poi, (poi) => poi.city, { nullable: true })
+	pois?: Poi[];
 
-  // a category can contain multiple ads
-  @Field(() => [User])
-  @OneToMany(() => User, (User) => User.city)
-  users: User[];
+	// a category can contain multiple ads
+	@Field(() => [User], { nullable: true })
+	@OneToMany(() => User, (User) => User.city, { nullable: true })
+	users?: User[];
 }
