37893448425bf5f2dca8660bfbd88e204e409de8
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-07-26 11:37:38 +0200

    updated reivews list after adding review and restricted review creation to connected users

--- frontend/src/pages/poi/[id].tsx
+++ frontend/src/pages/poi/[id].tsx
@@ -1,22 +1,77 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import {
 	Breadcrumbs,
 	Typography,
 	Grid,
 	Link,
 	CircularProgress,
+	Button,
+	TextField,
+	Alert,
+	Rating,
+	Box,
 } from "@mui/material";
-import { GET_POI_BY_ID } from "@queries";
+import { GET_ALL_RATINGS, GET_POI_BY_ID } from "@queries";
 import { PoiType } from "@types";
 import { mainTheme } from "@theme";
 import PlaceIcon from "@mui/icons-material/Place";
-import { ImagesCarousel, CreateReviewForm, ReviewList } from "@components";
+import { ImagesCarousel, RatingStars } from "@components";
 import AverageRating from "components/AverageRating";
+import { CREATE_REVIEW_MUTATION } from "@mutations";
+import { useAuth } from "context";
 
 const POIDetails = () => {
 	const router = useRouter();
+	const [rating, setRating] = useState<number | null>(null);
+	const [comment, setComment] = useState("");
+	const [errorText, setErrorText] = useState("");
+	const { user } = useAuth();
+	const [createReview, { loading: reviewLoading, error: reviewError, data: reviewData }] = useMutation(
+		CREATE_REVIEW_MUTATION
+	);
+const {
+	loading: reviewListLoading,
+	error: reviewListError,
+	data: reviewListData,
+	refetch: reviewListRefetch,
+} = useQuery(GET_ALL_RATINGS);
+
+	const { id, rating: urlRating } = router.query;
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		console.log(rating, comment);
+		if (!user) {
+			setErrorText("Pour laisser un commentaire, veuillez vous connecter.");
+			return;
+		}
+		e.preventDefault();
+		if (!rating || rating < 1 || rating > 5) {
+			setErrorText("Votre note doit être comprise entre 1 et 5");
+			return;
+		}
+		try {
+			await createReview({
+				variables: {
+					ratingData: {
+						rating,
+						text: comment,
+						user: user?.id,
+						poi: parseFloat(id as string),
+					},
+				},
+			});
+
+			setRating(null);
+			setComment("");
+			setErrorText("");
+
+			reviewListRefetch();
+		} catch (error) {
+			console.error("Error creating review:", error);
+		}
+	};
 	const [POI, setPOI] = useState<PoiType>({
 		name: "",
 		address: "",
@@ -55,7 +110,7 @@ const POIDetails = () => {
 		router.push(`/city/search/${POI.city}/category/${POI.category}`);
 	};
 
-	return loading ? (
+	return loading || reviewListLoading || reviewListLoading ? (
 		<CircularProgress />
 	) : (
 		<div>
@@ -141,8 +196,96 @@ const POIDetails = () => {
 					>
 						COMMENTAIRES
 					</Typography>
-					<CreateReviewForm />
-					<ReviewList />
+					{user && (
+						<>
+							<form>
+								<div>
+									<Rating
+										name="rating"
+										value={rating}
+										onChange={(event: any, newValue) => {
+											setRating(newValue);
+										}}
+									/>
+									{errorText && <p style={{ color: "red" }}>{errorText}</p>}
+								</div>
+								<div>
+									<TextField
+										required
+										label="Commentaire"
+										placeholder="Écrivez votre commentaire ici"
+										value={comment}
+										onChange={(e) => setComment(e.target.value)}
+										multiline
+										rows={4}
+										sx={{
+											backgroundColor: "white",
+											width: "100%",
+											"& .MuiOutlinedInput-root": {
+												"& fieldset": {
+													borderColor: mainTheme.palette.primary.main,
+												},
+											},
+										}}
+									/>
+								</div>
+
+								{reviewError && (
+									<Alert severity="error">Error: {error?.message}</Alert>
+								)}
+								{reviewData && (
+									<Alert severity="success">
+										Commentaire créé avec succès !
+									</Alert>
+								)}
+							</form>
+							<div
+								style={{
+									marginTop: "0.50rem",
+									display: "flex",
+									justifyContent: "flex-end",
+								}}
+							>
+								<Button
+									type="submit"
+									disabled={reviewLoading}
+									onClick={handleSubmit}
+									sx={{
+										textTransform: "none",
+										backgroundColor: mainTheme.palette.primary.dark,
+										color: "white",
+										"&:hover": {
+											backgroundColor: mainTheme.palette.primary.light,
+											color: mainTheme.palette.primary.dark,
+										},
+									}}
+								>
+									{reviewLoading ? <CircularProgress size={24} /> : "Ajouter"}
+								</Button>
+							</div>
+						</>
+					)}
+					{!user && <h5>Merci de vous connecter pour laisser un message</h5>}
+					<Box
+						sx={{
+							marginTop: "0.50rem",
+							maxHeight: "200px",
+							overflowY: "auto",
+							padding: "1rem",
+						}}
+					>
+						{reviewListData?.getAllRatings.map((review: any) => (
+							<Box key={review.id}>
+								<Typography>
+									<RatingStars rating={review.rating} />
+								</Typography>
+								<Typography>
+									{review.user.firstName} {review.user.lastName}
+								</Typography>
+								<Typography>{review.text}</Typography>
+							</Box>
+						))}
+					</Box>{" "}
 				</Grid>
 			</Grid>
 		</div>
